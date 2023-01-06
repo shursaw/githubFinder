5a2995e945625008cc7c2bfdc44e68c0d9cd475a
Olexander Savchenko <shursaw@gmail.com>
2023-01-06 23:54:16 +0200

    ready

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  "login": "welcome",
+  "avatar": "",
+  "name": "",
+  "company": "",
+  "blog": "",
+  "location": "",
+  "bio": "",
+  "twitter": "",
+  "repos": 0,
+  "followers": 10,
+  "following": 0,
+  "created": "0",
+}
